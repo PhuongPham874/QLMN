b61a915eea50daf785a9cc4a11056fafa4e0333e
PhanThao84 <phanthithanhthao842004123@gmail.com>
2025-05-06 10:58:38 +0700

    Thao lan 5

--- BHXH/apps.py
+++
@@ -1,6 +0,0 @@
-from django.apps import AppConfig
-
-
-class BhxhConfig(AppConfig):
-    default_auto_field = 'django.db.models.BigAutoField'
-    name = 'BHXH'
--- BHXH/urls.py
+++
@@ -1,9 +0,0 @@
-from django.urls import path
-from .views import *
-urlpatterns = [
-    path('bhxh/',bhxh,name='bhxh_list'),
-    path('addbhxh/',themmoiBHXH,name='add_bhxh'),
-    path('editbhxh/<int:ma_nv>',chinhsuaBHXH,name='edit_bhxh'),
-    path('infobhxh/<int:ma_nv>',thongtinchitiet,name='info_bhxh'),
-    path('noptien/',dong_bhxh,name='nop_tien')
-]
