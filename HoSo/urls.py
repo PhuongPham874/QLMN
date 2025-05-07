17af12287a6deadf4dc5c3a91d3877a2815f4c51
ttminhnguyet <minhnguyet170620044@gmail.com>
2025-05-07 09:07:21 +0700

    NguyetL2-7/5

--- HoSo/urls.py
+++ HoSo/urls.py
@@ -1,5 +1,6 @@
 
-from .views import danh_sach_nhan_vien, hosochitiet, them_moi_ho_so,edit_ho_so, nv_hosochitiet, them_moi_hop_dong,hosochitietHDLD, nv_hosochitietHDLD  # Chú ý tên hàm
+from .views import (danh_sach_nhan_vien, hosochitiet, them_moi_ho_so,edit_ho_so, nv_hosochitiet,
+                    them_moi_hop_dong,hosochitietHDLD, nv_hosochitietHDLD, edit_hdld)  # Chú ý tên hàm
 from django.conf import settings
 from django.conf.urls.static import static
 from django.urls import path
@@ -8,6 +9,7 @@ urlpatterns = [
     path('hosochitiet/<int:id>/', hosochitiet, name='QLXemHoSo'),
     path('hosochitiet/<int:id>/edit/', edit_ho_so, name='edit_ho_so'),
     path('hosochitietHDLD/<int:nhan_vien_id>/', hosochitietHDLD, name='QLXemHoSoHDLD'),
+    path('hosochitietHDLD/<int:nhan_vien_id>/edit/', edit_hdld, name='edit_hdld'),
     path('themmoihoso/', them_moi_ho_so, name='ThemMoiHoSo'),
     path('themmoi_hdld/<int:nhan_vien_id>/', them_moi_hop_dong, name='themmoi_hdld'),  # Đảm bảo tên đúng
     path('nvhosochitiet/', nv_hosochitiet, name="NVXemHoSo"),
--- HoSo/views.py
+++ HoSo/views.py
@@ -1,8 +1,9 @@
 from django.contrib.auth.decorators import login_required
 from django.shortcuts import render, get_object_or_404, redirect
-from HOME.models import NhanVien, HopDongLaoDong, PhuCapNhanVien
+from HOME.models import NhanVien, HopDongLaoDong, PhuCapNhanVien, User
 from datetime import datetime
 from django.contrib.auth.models import User
+from .forms import NhanVienForm, HopDongLaoDongForm
 
 
 
@@ -31,23 +32,28 @@ def hosochitiet(request, id):
     context = {'nhan_vien': nhan_vien, 'hop_dong': hop_dong}
 
     return render(request, 'HoSo/hosochitiet.html', context)
-
+'''
 def danh_sach_nhan_vien(request):
     nhan_vien_list = NhanVien.objects.all()
     context = {
         'nhan_vien_list': nhan_vien_list,
     }
     return render(request, 'HoSo/danhsachnhanvien.html', context)
+'''
 
 
 
+def danh_sach_nhan_vien(request):
+    query = request.GET.get('q', '')  # Lấy giá trị tìm kiếm từ thanh tìm kiếm
+    if query:
+        nhan_vien_list = NhanVien.objects.filter(ten_nv__icontains=query)  # Lọc nhân viên theo tên
+    else:
+        nhan_vien_list = NhanVien.objects.all()  # Nếu không có tìm kiếm, hiển thị tất cả nhân viên
 
-
-
-from django.shortcuts import render, redirect
-from django.http import HttpResponse
-from .forms import NhanVienForm, HopDongLaoDongForm
-from HOME.models import NhanVien, User
+    return render(request, 'HoSo/danhsachnhanvien.html', {
+        'nhan_vien_list': nhan_vien_list,
+        'query': query
+    })
 
 
 def them_moi_ho_so(request):
@@ -142,22 +148,47 @@ from .forms import NhanVienForm
 def edit_ho_so(request, id):
     # Lấy nhân viên theo ID
     nhan_vien = get_object_or_404(NhanVien, id=id)
-
     # Nếu là POST request, nghĩa là người dùng đã nhấn Lưu
     if request.method == 'POST':
         form = NhanVienForm(request.POST, request.FILES, instance=nhan_vien)
-
         # Kiểm tra tính hợp lệ của form
         if form.is_valid():
             form.save()  # Lưu thông tin vào cơ sở dữ liệu
             return redirect('QLXemHoSo', id=nhan_vien.id)  # Chuyển hướng đến trang xem hồ sơ chi tiết
-
         else:
             print("Form không hợp lệ")
-            print(form.errors)  # Xem các lỗi của form để debug
-
+            print(form.errors)  # Xem các lỗi của form để debu
     else:
         form = NhanVienForm(instance=nhan_vien)
 
     # Render lại trang với form
     return render(request, 'HoSo/edit_ho_so.html', {'form': form, 'nhan_vien': nhan_vien})
+
+
+from django.shortcuts import render, redirect, get_object_or_404
+from HOME.models import NhanVien, HopDongLaoDong
+from .forms import HopDongLaoDongForm  # Form chỉnh sửa hợp đồng lao động (nếu chưa có)
+
+def edit_hdld(request, nhan_vien_id):
+    # Lấy thông tin nhân viên và hợp đồng lao động của nhân viên đó
+    nhan_vien = get_object_or_404(NhanVien, id=nhan_vien_id)
+    hop_dong = get_object_or_404(HopDongLaoDong, nhan_vien=nhan_vien)
+
+    # Kiểm tra nếu yêu cầu là POST (lưu thông tin)
+    if request.method == "POST":
+        form = HopDongLaoDongForm(request.POST, instance=hop_dong)
+
+        # Kiểm tra form hợp lệ
+        if form.is_valid():
+            form.save()  # Lưu thông tin hợp đồng vào cơ sở dữ liệu
+            return redirect('QLXemHoSoHDLD', nhan_vien_id=nhan_vien.id)  # Quay lại trang xem hợp đồng
+
+    else:
+        # Tạo form với dữ liệu hiện tại của hợp đồng lao động
+        form = HopDongLaoDongForm(instance=hop_dong)
+
+    return render(request, 'HoSo/edit_hdld.html', {
+        'form': form,
+        'nhan_vien': nhan_vien,
+        'hop_dong': hop_dong
+    })
