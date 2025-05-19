e0b183105a591fa86ec562ec24d1bd294e3d9c63
giapdoan01 <gdoan012@gmail.com>
2025-05-19 18:48:26 +0700

    done 19/5 - login, changepassword

--- HOME/views.py
+++ HOME/views.py
@@ -44,3 +44,26 @@ def logout_view(request):
     logout(request)
     return redirect('login')
 
+from django.shortcuts import render, redirect
+from django.contrib.auth.decorators import login_required
+from django.contrib import messages
+from django.contrib.auth import update_session_auth_hash
+from django.contrib.auth.forms import PasswordChangeForm
+@login_required
+def change_password(request):
+    if request.method == 'POST':
+        form = PasswordChangeForm(request.user, request.POST)
+        if form.is_valid():
+            user = form.save()
+            update_session_auth_hash(request, user)  # Đảm bảo session auth không bị logout
+            messages.success(request, 'Mật khẩu của bạn đã được thay đổi thành công!')
+            return redirect('home')  # Điều hướng sau khi đổi mật khẩu thành công
+        else:
+            messages.error(request, 'Vui lòng sửa các lỗi bên dưới.')
+    else:
+        form = PasswordChangeForm(request.user)
+
+    return render(request, 'changepassword.html', {
+        'form': form
+    })
+
